46b84d3f431335778bd4e01a665ef173c08b2fb5
muratcnshn24 <127681337+muratcnshn24@users.noreply.github.com>
2024-07-18 18:27:01 +0300

    Proje dosyası ekle.

---
+++ carpim_tablosu_olusturan_program(DNGS)/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace carpim_tablosu_olusturan_program_DNGS_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+
+                for (int j = 1; j <= 10; j++)
+                {
+                    Console.WriteLine("{0,5} \t x {1,5}={2,5}", i, j, i*j);
+                }
+                Console.WriteLine("\n\n------------------------------------------\n\n");
+            }
+        }
+    }
+}
